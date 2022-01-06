afb771e6242fdadd10c833a30ead3bbb77cf8245
C. Vilsmeier <vilsmeier@gmx.de>
2022-01-06 17:25:23 +0100

    foo

--- build.sh
+++ build.sh
@@ -4,14 +4,17 @@ rm -rf bin
 mkdir -p bin
 
 echo gcc
+which gcc
 gcc -std=c99 -static -o bin/camp main.c
 echo gcc exitcode $?
 
 echo file
+which file
 file bin/camp
 echo file exitcode $?
 
 echo ldd
+which ldd
 ldd bin/camp
 echo ldd exitcode $?
 
